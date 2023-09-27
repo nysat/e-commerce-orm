e6d4688280dc4c2b6a56d1d5f72972e515839060
nysat <nysa-cere3@hotmail.com>
2023-09-27 02:13:36 -0400

    added product columns in product.js

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,39 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type:DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    stock: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 15,
+      primaryKey: true,
+      validate: {
+        isNumeric: true,
+      },
+    category_id: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
+    },
   },
   {
     sequelize,
